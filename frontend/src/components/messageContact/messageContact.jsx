a8fa48f53f5051311e5f4422167b9e76b73ba74f
Deividokun <129647532+Deividokun@users.noreply.github.com>
2025-05-14 11:03:57 +0200

    Update messageContact.jsx

---
+++ frontend/src/components/messageContact/messageContact.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './messageContact.css'
+
+function SuccessMessage({ onClose }) {
+  return (
+    <div className='success-overlay'>
+      <div className='success-box'>
+        <p>Message sent successfully!</p>
+        <button onClick={onClose}>Cerrar</button>
+      </div>
+    </div>
+  )
+}
+
+export default SuccessMessage
